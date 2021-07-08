063eb9c40abc320919b074a5767a71743faeea0f
Akshay <turkarakshay186@gmail.com>
2021-07-08 14:17:57 +0530

    bug fixed

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ let len = 0;
 let i = 0;
 var points = 0;
 const text = "Total:";
+let o = g = v = a = y = p = w1 = w2 = w3 = 1; 
 function orange() {
+	
+	if(o === 1)
+	{
 	 color = document.getElementsByClassName("orange");
 	 len = color.length;
 	 for(i=0; i<len ; i++){
@@ -11,26 +15,41 @@ function orange() {
 	 	points += 5;
 	 }
 	 document.getElementById("total").innerHTML = text +	points;
+	 o++;
+	}
 }
+
 function green() {
-	 color = document.getElementsByClassName("green");
+	
+	if(g === 1)
+	{
+	color = document.getElementsByClassName("green");
 	 len = color.length;
 	 for(i=0; i<len ; i++){
 	 	color[i].style.background = "#00ff00";
 	 	points += 7;
 	 }
 	 document.getElementById("total").innerHTML = text +	points;
+	 g++;
+	} 
 }
+
 function voilet() {
-	 color = document.getElementsByClassName("voilet");
+	
+	if(v === 1){
+		color = document.getElementsByClassName("voilet");
 	 len = color.length;
 	 for(i=0; i<len ; i++){
 	 	color[i].style.background = "#5200cc";
 	 	points += 8;
 	 }
 	 document.getElementById("total").innerHTML = text +	points;
+	 v++;
+	} 
 }
 function aqua() {
+	
+	if(a === 1){
 	 color = document.getElementsByClassName("aqua");
 	 len = color.length;
 	 for(i=0; i<len ; i++){
@@ -38,8 +57,13 @@ function aqua() {
 	 	points += 15;
 	 }
 	 document.getElementById("total").innerHTML = text +	points;
+	 a++;
+	}
 }
+
 function pink() {
+	
+	if(p === 1){
 	 color = document.getElementsByClassName("pink");
 	 len = color.length;
 	 for(i=0; i<len ; i++){
@@ -47,8 +71,12 @@ function pink() {
 	 	points += 5;
 	 }
 	 document.getElementById("total").innerHTML = text +	points;
+	 p++;
+	}
 }
 function yellow() {
+	
+	if(y === 1){
 	 color = document.getElementsByClassName("yellow");
 	 len = color.length;
 	 for(i=0; i<len ; i++){
@@ -56,30 +84,48 @@ function yellow() {
 	 	points += 10;
 	 }
 	 document.getElementById("total").innerHTML = text +	points;
+	 y++;
+	}
 }
 function white1() {
+	
+	if(w1 === 1){
 	color = document.getElementById("white1");
 	color.style.background = "#ffffff";
 	points += 25;
-	document.getElementById("total").innerHTML = text +	points;	
+	document.getElementById("total").innerHTML = text +	points;
+	w1++;	
+	}
 }
 function white2() {
+	w2 = 1;
+	if(w2 === 1){
 	color = document.getElementById("white2");
 	color.style.background = "#ffffff";
 	points += 25;
-	document.getElementById("total").innerHTML = text +	points;	
+	document.getElementById("total").innerHTML = text +	points;
+	 w2++;	
+	}
 }
 function white3() {
+	
+	if(w3 === 1){
 	color = document.getElementById("white3");
 	color.style.background = "#ffffff";
 	points += 25;
 	document.getElementById("total").innerHTML = text +	points;	
+	}
+	w3++;
 }
 function white4() {
+	
+	if(w4 === 1){
 	color = document.getElementById("white4");
 	color.style.background = "#ffffff";
 	points += 25;
-	document.getElementById("total").innerHTML = text +	points;	
+	document.getElementById("total").innerHTML = text +	points;
+	 w4++;	
+	}
 }
 function cross() {
 	 color = document.getElementsByClassName("cross");
